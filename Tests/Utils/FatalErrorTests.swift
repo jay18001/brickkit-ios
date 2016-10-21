af9d5bff5e8c0e4a974c7572c8f27d347fe94f1d
Ruben Cagnie <rcagnie@wayfair.com>
2016-10-20 22:37:47 -0400

    dispatch_sync

--- Tests/Utils/FatalErrorTests.swift
+++ Tests/Utils/FatalErrorTests.swift
@@ -11,12 +11,41 @@ import XCTest
 
 private var locked = false
 private let timeInterval: NSTimeInterval = 2
+private let lockQueue = dispatch_queue_create("com.test.LockQueue", nil)
 
 extension XCTestCase {
 
     func expectFatalError(expectedMessage: String? = nil, testcase: () -> Void) {
-        return
-        
+
+        dispatch_sync(lockQueue) {
+            // code
+            // arrange
+            let expectation = expectationWithDescription("expectingFatalError")
+            var assertionMessage: String? = nil
+
+            // override fatalError. This will pause forever when fatalError is called.
+            FatalErrorUtil.replaceFatalError { message, _, _ in
+                assertionMessage = message
+                expectation.fulfill()
+            }
+
+            // act, perform on separate thead because a call to fatalError pauses forever
+            dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), testcase)
+
+            waitForExpectationsWithTimeout(5) { _ in
+                if let message  = expectedMessage {
+                    // assert
+                    XCTAssertEqual(assertionMessage, message)
+                }
+
+                // clean up
+                FatalErrorUtil.restoreFatalError()
+            }
+        }
+    }
+
+    func _expectFatalError(expectedMessage: String? = nil, testcase: () -> Void) {
+
         repeat {
             if !NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: timeInterval)) {
                 NSThread.sleepForTimeInterval(timeInterval)
@@ -46,7 +75,7 @@ extension XCTestCase {
 
             // clean up
             FatalErrorUtil.restoreFatalError()
-
+            
             locked = false
         }
     }
